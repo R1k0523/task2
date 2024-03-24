76e6bcfdb528556224a859f24625bca434ee80f7
R1k0523 <isergey555345@gmail.com>
2024-03-24 13:37:39 +0300

    Initial commit

---
+++ src/test/kotlin/org/example/task1proginj/Task1proginjApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.task1proginj
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Task1proginjApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
